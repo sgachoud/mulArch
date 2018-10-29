97c5cd2f0b632744acd1e0a29c7d915c951f5bf5
Martino <m.milani@l2f.ch>
2018-10-29 19:14:58 +0100

    corrected bug in indices, now starting from 0

--- A2/algorithm.c
+++ A2/algorithm.c
@@ -30,7 +30,7 @@ void simulate(double *input, double *output, int threads, int length, int iterat
         // dividing into vertical blocks whose width fit in a 64B line cache
         int blocks = length / doubles_per_block;
 
-        // initialize block_width with N
+        // initialize block_width with doubles_per_block
         int block_width = doubles_per_block;
 
         // looping over blocks
@@ -40,16 +40,19 @@ void simulate(double *input, double *output, int threads, int length, int iterat
                 block_width += length % doubles_per_block;
 
             // looping over rows
-            for(int i=1; i<n; i++)
+            for(int i=0; i<n; i++)
             {
                 // looping over columns
-                for(int h=1; h<block_width; h++)
+                // h identifies the h-th columns of the block b
+                for(int h=0; h<block_width; h++)
                 {
+                    // j identifies the corresponding index inside the whole matrix
                     j = b*doubles_per_block + h;
+
+                    // implementation of the algorithm
                     if ( ((i == length/2-1) || (i== length/2))
                         && ((j == length/2-1) || (j == length/2)) )
                         continue;
-
                     OUTPUT(i,j) = (INPUT(i-1,j-1) + INPUT(i-1,j) + INPUT(i-1,j+1) +
                                    INPUT(i,j-1)   + INPUT(i,j)   + INPUT(i,j+1)   +
                                    INPUT(i+1,j-1) + INPUT(i+1,j) + INPUT(i+1,j+1) )/9;
